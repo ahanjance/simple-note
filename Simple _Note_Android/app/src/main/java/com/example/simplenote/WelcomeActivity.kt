0dee61ecef479170dc813f27f6f256a5654f1d80
V <94721740+ahanjance@users.noreply.github.com>
2025-09-11 02:30:36 +0330

    feat: basic start navigation

--- Simple _Note_Android/app/src/main/java/com/example/simplenote/WelcomeActivity.kt
+++ Simple _Note_Android/app/src/main/java/com/example/simplenote/WelcomeActivity.kt
@@ -1,13 +1,21 @@
 package com.example.simplenote
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
-
+import com.google.android.material.button.MaterialButton
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // Find the Get Started button and set a click listener
+        val getStartedButton = findViewById<MaterialButton>(R.id.getStartedButton)
+        getStartedButton.setOnClickListener {
+            // Navigate to LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
